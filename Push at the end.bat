4948c35a5fd980eef299bbc420f4ac3b4d396502
Anton250 <antosha250900@gmail.com>
2018-09-22 13:54:49 +0300

    New repo
    Signed-off-by: Anton250 <antosha250900@gmail.com>

---
+++ Push at the end.bat
@@ -0,0 +1,4 @@
+git add *.cpp *.sln *.vcxproj *.vcxproj.filters *.h *.bat *.user
+git commit -sm "New repo"
+git push Repo master
+pause
---
+++ auto_add.bat
@@ -0,0 +1,3 @@
+git add *.cpp *.sln *.vcxproj *.vcxproj.filters *.h
+git commit -sm "Some comment"
+pause
